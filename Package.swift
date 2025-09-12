a7375e95900e55090e6740ced9f5f675168caddb
dyte-devel <76557270+dyte-devel@users.noreply.github.com>
2025-09-12 08:12:38 +0000

    chore(release): update SPM definition

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 import PackageDescription
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://sdk-assets.realtime.cloudflare.com/RealtimeKitFlutterCoreKMM-unspecified-3cb1ccf2-8482-45b8-a707-892e63750571.xcframework.zip"
-let remoteKotlinChecksum = "e654ffacf8240bd03ea36de362432cbd8e3cbb0bf17771b99a84439958653c9c"
+let remoteKotlinUrl = "https://sdk-assets.realtime.cloudflare.com/RealtimeKitFlutterCoreKMM-unspecified-1f449485-6dc5-4cff-961a-2416ceb9c60f.xcframework.zip"
+let remoteKotlinChecksum = "ded70b1ba9f0fa2517591c91e908d7c002ccc0546f02dc52d67acdf78783f115"
 let packageName = "RealtimeKitFlutterCoreKMM"
 // END KMMBRIDGE BLOCK
 
